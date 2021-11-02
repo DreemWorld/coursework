14c79eb7d2b2b2727e1bdcb97fdfc3dc4dfbef46
Kirill Vovk <kirill.vovk.2002@mail.ru>
2021-11-02 21:56:19 +0300

    add: app

---
+++ app/app/src/main/java/course/work/kishlist/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package course.work.kishlist.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
