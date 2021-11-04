852a552c9eae97392e538563c940e41ab75d772b
Kirill Vovk <kirill.vovk.2002@mail.ru>
2021-11-04 09:13:30 +0300

    add: application

--- app/app/src/main/java/course/work/kishlist/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package course.work.kishlist.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
