facf02d94b70bbcd46fa7950b00ec26b9ba07a34
Kirill Vovk <kirill.vovk.2002@mail.ru>
2021-12-01 23:43:07 +0300

    add: create db users + child id & name

--- kishlish/app/src/main/java/coursework/gi/kishlish/ui/fragments/Sign.kt
+++ kishlish/app/src/main/java/coursework/gi/kishlish/ui/fragments/Sign.kt
@@ -1,6 +1,7 @@
 package coursework.gi.kishlish.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,7 @@ import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 import coursework.gi.kishlish.R
 import coursework.gi.kishlish.databinding.ActivityMainBinding
@@ -22,7 +24,7 @@ class Sign : Fragment(R.layout.fragment_sign) {
 
     private lateinit var binding: FragmentSignBinding
     private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var daatabaseReference: DatabaseReference
+    private lateinit var databaseReference: DatabaseReference
     private val navController by lazy { findNavController() }
 
     override fun onCreateView(
@@ -47,12 +49,20 @@ class Sign : Fragment(R.layout.fragment_sign) {
             ).addOnCompleteListener {
                 if (it.isSuccessful) {
                     val uid = firebaseAuth.currentUser?.uid.toString()
-                    var dateMap = mutableMapOf<String, Any>()
+                    val dateMap = mutableMapOf<String, Any>()
                     dateMap["id"] = uid
-                    dateMap["name"] = binding.editTextUsernameSign.text
-                    navController.navigate(R.id.action_sign_to_login)
-                } else {
-                    Toast.makeText(activity, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    dateMap["name"] = binding.username.text.toString()
+                    databaseReference = FirebaseDatabase.getInstance().reference
+                    databaseReference.child("users").child(uid).updateChildren(dateMap)
+                        .addOnCompleteListener { it2 ->
+                            if (it2.isSuccessful) {
+                                navController.navigate(R.id.action_sign_to_login)
+                            } else {
+                                Toast.makeText(activity, it2.exception?.message, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    } else {
+                        Toast.makeText(activity, it.exception?.message, Toast.LENGTH_SHORT).show()
                 }
             }
         }
